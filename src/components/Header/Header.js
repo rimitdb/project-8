7214228ac7736a541d7cb6f5ed8c275d4391a000
Iqbal <iqbal.du.mis@gmail.com>
2022-03-26 19:32:25 +0600

    Mobile Responsive Done

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,7 +4,7 @@ import './Header.css';
 const Header = () => {
     return (
         <div className='header'>
-            <h1>Top Travel Destination</h1>
+            <h2>Top Travel Destination</h2>
         </div>
     );
 };
